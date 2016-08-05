073f60bb227c79030b7cc042124a50df67b62458
tanbobo <tandingbo@163.com>
2016-08-05 16:46:22 +0800

    tanbobo 提交之内容

--- src/main/java/com/tanbobo/platfrom/base/common/util/DateUtils.java
+++ src/main/java/com/tanbobo/platfrom/base/common/util/DateUtils.java
@@ -1,6 +1,8 @@
 package com.tanbobo.platfrom.base.common.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -18,5 +20,50 @@ public class DateUtils {
     public static String format(Date date, String str) {
         SimpleDateFormat sdf = new SimpleDateFormat(str);
         return sdf.format(date);
+    }
+
+
+    /**
+     * 获得指定日期的前一天
+     *
+     * @param specifiedDay
+     * @return
+     */
+    public static String getSpecifiedDayBefore(String specifiedDay) {
+        Calendar c = Calendar.getInstance();
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yy-MM-dd").parse(specifiedDay);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day - 1);
+
+        String dayBefore = new SimpleDateFormat("yyyy-MM-dd").format(c.getTime());
+        return dayBefore;
+    }
+
+    /**
+     * 获得指定日期的后一天
+     *
+     * @param specifiedDay
+     * @return
+     */
+    public static String getSpecifiedDayAfter(String specifiedDay) {
+        Calendar c = Calendar.getInstance();
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yy-MM-dd").parse(specifiedDay);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day + 1);
+
+        String dayAfter = new SimpleDateFormat("yyyy-MM-dd").format(c.getTime());
+        return dayAfter;
     }
 }
